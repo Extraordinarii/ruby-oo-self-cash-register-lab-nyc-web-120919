10f984b3b48ebaee33eb5dc3fb72e92962bde408
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-11 14:54:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+     attr_accessor :total, :discount, :cart, :last_transaction
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end 
+  
+  def add_item(title,price,quantity = 1) #def takes in title and price and quantity that has an initial value of 1 
+     @total += price * quantity
+      quantity.times do 
+        @cart << title
+      end
+  end 
+  
+  def apply_discount
+    @total
+  end 
+  
+end 
 
